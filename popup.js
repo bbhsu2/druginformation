1559a3592f1ac5999eb0e8a84b1afb8161b0ed1f
Bernard Hsu <bbhsu2@hotmail.com>
2014-09-03 17:32:44 -0500

    adding tab navigation, adding more sources and new icon

--- popup.js
+++ popup.js
@@ -1,6 +1,51 @@
+/* Bernard Hsu, 2014
+License information here */
+
+var bg = chrome.extension.getBackgroundPage();
+var LOG_SRC = "POPUP";
+var searchStr = "";
+
+// Simple little timer class to help with optimizations
+function Timer() {
+  this.start = (new Date).getTime();
+  this.last = this.start;
+}
+Timer.prototype.log = function(id) {
+  var now = (new Date).getTime();
+  log(id + " total time " + (now - this.start) + " m/s, delta " + (now - this.last) + " m/s");
+  this.last = now;
+};
+
+/**
+ * Log call that prepends the LOG_SRC before delegating to the background page to simplify debugging
+ */
+function log() {
+  var args = Array.prototype.slice.call(arguments);
+  args.unshift(LOG_SRC);
+  bg.log.apply(bg, args);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-	$(":button")[0].addEventListener('click', searchClick);
-})
+	var allLabels = $("label");
+	var flag = false; //TODO: fix this so it's based on checkbox state.
+	$("div").find(allLabels)[0].addEventListener('click', function(e){
+		if(flag) {
+			$(this).html("Check All");
+			$(".checkboxOption").each(function(){
+				this.checked = false;
+			});
+			flag = false;
+		} else{
+			$(this).html("Unselect All");
+			$(".checkboxOption").each(function(){
+				this.checked = true;
+			});
+			flag =  true;
+		}
+	});
+	
+	$(":button")[1].addEventListener('click', searchClick);
+ })
 
 function getPubMedLink(query){
 	return "http://www.ncbi.nlm.nih.gov/pubmed/?term=" + query;
@@ -10,6 +55,10 @@ function getDailyMedLink(query){
 	return "https://dailymed.nlm.nih.gov/dailymed/search.cfm?startswith=" +
 		   query +
 		   "&x=0&y=0";
+}
+
+function getClinPharmLink(query){
+	return "http://clinicalpharmacology-ip.com/Forms/search.aspx?s=" + query;
 }
 
 function getDrugsAtFDALink(query){
@@ -23,7 +72,22 @@ function getGoogleLink(query){
 	return "https://www.google.com/#q=" + query;
 }
 
-function searchClick(){
+function getClinicalTrialsGovLink(query){
+	return "http://clinicaltrials.gov/ct2/results?term=" + query + "&Search=Search";
+}
+
+function getEMEALink(query){
+	return "http://www.ema.europa.eu/ema/index.jsp " +
+	"?curl=pages/medicines/landing/epar_search.jsp" +
+	"&mid=WC0b01ac058001d124" +
+	"&source=homeMedSearch" +
+	"&keyword=" + query +
+	"&category=human" +
+	"&isNewQuery=true";
+}
+
+
+function searchClick(e){
 	var query = document.getElementById("searchQuery").value;
 	var boxes = $(":checkbox");
 	
@@ -32,18 +96,34 @@ function searchClick(){
 		if(boxes[i].checked){
 			switch(i){
 				case 0:
-					chrome.tabs.create({ url: getDailyMedLink(query) });
+					chrome.tabs.create({url : getClinPharmLink(query)});
 					break;
 				case 1:
-					chrome.tabs.create({ url: getDrugsAtFDALink(query) });
+					chrome.tabs.create({url: getClinicalTrialsGovLink(query) });
 					break;
 				case 2:
-					chrome.tabs.create({ url: getGoogleLink(query) });
+					chrome.tabs.create({ url: getDailyMedLink(query) });
 					break;
 				case 3:
+					chrome.tabs.create({ url: getDrugsAtFDALink(query) });
+					break;
+				case 4:
+					chrome.tabs.create({ url: getEMEALink(query) });
+					break;
+				case 5:
+					chrome.tabs.create({ url: getGoogleLink(query) });
+					break;
+				case 6:
 					chrome.tabs.create({ url: getPubMedLink(query) });
 					break;
 			}
 		}
 	}
+}
+
+function checkAll(e){
+	
+	$(".checkboxOption").each(function(){
+		this.checked = true;
+	});
 }
